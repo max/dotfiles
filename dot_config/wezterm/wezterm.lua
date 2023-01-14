23ad2e2a2e8279edf54fc52eaa472547ce83f9b1
Max Schoening <max@max.wtf>
2023-01-14 15:33:05 -0800

    Add configuration for Wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+return {
+	-- color_scheme = "Dark Violet (base16)",
+	color_scheme = "Framer (base16)",
+	colors = {
+		tab_bar = {
+			active_tab = {
+				bg_color = "#181818",
+				fg_color = "#ffffff",
+			},
+		},
+	},
+}
