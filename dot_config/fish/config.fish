9224e3b2a9b4879108b1e168c7b924a10ee33dba
Max Schoening <max@max.dev>
2025-10-05 13:46:35 -0700

    Add cargo and rust tools to path

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,10 +1,11 @@
 set -U fish_greeting
 
-fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.bun/bin
+fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
+fish_add_path /opt/homebrew/bin
 
 if status is-interactive
-  atuin init fish | source
-  zoxide init fish | source
+    atuin init fish | source
+    zoxide init fish | source
 end
