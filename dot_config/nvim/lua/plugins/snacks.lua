527583c0ed76271d81450d5374d8f86fc47f6608
Max Schoening <max@max.dev>
2025-12-13 17:41:50 -0800

    Disable visually prominent indent guides

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+      indent = {
+        animate = { enabled = false },
+        scope = {
+          hl = "SnacksIndentScopeSubtle",
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Set up snacks with options
+      require("snacks").setup(opts)
+
+      -- Use a more subtle gray for indent guides (works well with Gruvbox)
+      vim.api.nvim_set_hl(0, "SnacksIndentScopeSubtle", { fg = "#3c3836" })
+    end,
+  },
+}
